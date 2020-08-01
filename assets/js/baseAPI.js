2079f8d43615d5abd482c83a4393b8eaa66745e9
刘佳琦 <603070162@qq.com>
2020-08-01 12:43:40 +0800

    完成个人中心功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -7,14 +7,14 @@ $.ajaxPrefilter(function(options){
             Authorization:localStorage.getItem('token')
         }
     }
-    //全局统一挂载complete回调函数
-    options.complete=function(res){
-        if (res.responseJSON.status === 1 && res.responseJSON.
-            message === '身份认证失败！') {
-            // 1. 强制清空 token
-            localStorage.removeItem('token')
-            // 2. 强制跳转到登录页面
-            location.href = '/login.html'
-          }
-    }
+    // //全局统一挂载complete回调函数
+    // options.complete=function(res){
+    //     if (res.responseJSON.status === 1 && res.responseJSON.
+    //         message === '身份认证失败！') {
+    //         // 1. 强制清空 token
+    //         localStorage.removeItem('token')
+    //         // 2. 强制跳转到登录页面
+    //         location.href = '/login.html'
+    //       }
+    // }
 })
